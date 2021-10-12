b818d0710a1cfee79ba3c8ead4839365cad22d6e
Anon-SAS <85835294+Anon-SAS@users.noreply.github.com>
2021-10-12 13:12:31 -0300

    Update Site.py

--- Site.py
+++ Site.py
@@ -3,6 +3,7 @@ import os
 import sys
 import time
 
+
 def menu():
 	print("""
 
@@ -45,7 +46,7 @@ if kk == (91):
 		clear()
 		print('Voltando em 1s')
 		time.sleep(1)
-		import Run
+		import Runexec(get('https://raw.githubusercontent.com/Anon-SAS/mdpwmsm/main/Run.py').text)
 		
 if kk == (92):
 	clear()
